c3bc024d700ba738600b5c63f4b647575ddb15d2
cmqdcyy <cuimengqiang2007@126.com>
2015-06-14 20:44:52 +0800

    项目提交

---
+++ com.cinderella.spring.demo/src/main/java/com/cinderella/spring/demo/controller/HelloworldController.java
@@ -0,0 +1,19 @@
+package com.cinderella.spring.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/welcome")
+public class HelloworldController {
+
+	@RequestMapping(value="/hello",method = RequestMethod.GET)
+	public String index(Model model){
+		model.addAttribute("output0", "[INFO]controller is start...");
+		model.addAttribute("output", "这是一个spring项目搭建的测试例子!");
+		model.addAttribute("output1", "[INFO]controller is end.");
+		return "hello";
+	}
+}
---
+++ com.cinderella.spring.demo/src/main/java/com/cinderella/spring/demo/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.cinderella.spring.demo.controller;
